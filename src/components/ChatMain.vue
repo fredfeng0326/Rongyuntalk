d012f7bf0f6c2c31d36a838d2524615c8a2e2f9d
fred <fred@9an.com>
2018-07-23 17:43:37 +0800

    Initial commit

---
+++ src/components/ChatMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-aside>
+      <Card></Card>
+    </el-aside>
+    <el-main>this is main</el-main>
+  </el-container>
+</template>
+
+<script>
+import Card from '@/components/card.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <header>
+      <img class="avatar" width="40" height="40" :alt="user.name" :src="user.img">
+      <p class="name">fred</p>
+    </header>
+    <footer>
+      <input class="search" type="text" placeholder="search user...">
+    </footer>
+  </div>
+</template>
+
+<script>
+import { actions } from '@/store/index.js'
+export default {
+  vuex: {
+    actions: actions,
+    getters: {
+      user: ({ user }) => user,
+      filterKey: ({ filterKey }) => filterKey
+    }
+  },
+  methods: {
+    onKeyup (e) {
+      this.search(e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
